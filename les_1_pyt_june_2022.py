31ffd1dc7e99038dcc8ca119eddf2e4c2391489d
romankt21 <romankt21@gmail.com>
2022-11-19 23:26:57 +0200

    change

--- les_1_pyt_june_2022.py
+++ les_1_pyt_june_2022.py
@@ -208,7 +208,8 @@ kkh
 
 
 
-# #tuple кортеж - це список констант, яких не можна змінювати, записуються в круглих дужках. Не можна ні додавати, ні віднімати, можна підрахувати і витягнути елемент, знайти індекс елементу. Можна тільки витягнути з api
+# #tuple кортеж - це список констант, яких не можна змінювати, записуються в круглих дужках. Не можна ні додавати,
+# # ні віднімати, можна підрахувати і витягнути елемент, знайти індекс елементу. Можна тільки витягнути з api
 
 # my_tuple = (1, 2, 3, 4, 5)
 
@@ -227,7 +228,8 @@ kkh
 #
 # print(d) # {'name': 'max', 'age': 15}
 #
-# c = {'name': 'max', 'age' : 15} # якщо ключ name текстового формату його теж потрібно брати в лапки, ключем виступає стрічка або кортеж
+# c = {'name': 'max', 'age' : 15} # якщо ключ name текстового формату його теж потрібно брати в лапки, ключем
+# виступає стрічка або кортеж
 # print(c) # {'name': 'max', 'age': 15}
 
 # user = {
@@ -278,12 +280,14 @@ kkh
 # print(user.values()) # повертає список значень
 # print(user)    # dict_values(['Max', 38, 'male'])  # {'name': 'Max', 'age': 38, 'gender': 'male'}
 
-# setdefault_value = user.setdefault('key', 'value_my_key') # дозволяє додавати ключ якого немає і за бажанням значення. додає ключ, якщо такого немає, якщо ключ є він його не додає.
+# setdefault_value = user.setdefault('key', 'value_my_key') # дозволяє додавати ключ якого немає і за бажанням значення.
+# додає ключ, якщо такого немає, якщо ключ є він його не додає.
 # print(setdefault_value) # повертає значення value_my_key
 # print(user) # {'name': 'Max', 'age': 15, 'gender': 'male', 'key': 'value_my_key'}
 
 
-# setdefault_value = user.setdefault('age', 'value_my_key') # якщо хочемо замінити значення в наявному ключі, то це значення не змігиться і залишиться попереднє значення
+# setdefault_value = user.setdefault('age', 'value_my_key') # якщо хочемо замінити значення в наявному ключі,
+# то це значення не змігиться і залишиться попереднє значення
 # print(setdefault_value) # 15
 # print(user) # {'name': 'Max', 'age': 15, 'gender': 'male'}
 
@@ -294,7 +298,8 @@ kkh
 
 # l = [1, 2, 3, 4, 6, 2, 5, 4]
 #
-# s = set(l) # передаємо в змінну s свій список, який тепер буде set, при цьому всі дублікати в списку видаляються і залишаються тільки унікальні значення (без повторів)
+# s = set(l)    # передаємо в змінну s свій список, який тепер буде set, при цьому всі дублікати в списку видаляються
+#                   # і залишаються тільки унікальні значення (без повторів)
 #
 # print(s)  # {1, 2, 3, 4, 5, 6}
 
@@ -334,22 +339,25 @@ kkh
 # print(set1)             #  {1, 3, 4, 5, 6}
 
 
-# print(set1.issuperset(set2))       #  чи є в set1 всі елементи які є в set2  # False  # оскільки в set1 є 3, 4, 5, але немає 8 і 10
+# print(set1.issuperset(set2))       #  чи є в set1 всі елементи які є в set2  # False  # оскільки в set1 є 3, 4, 5,
+                                        #   # але немає 8 і 10
 #
 # set3 = {1, 2 , 3}
 # print(set1.issuperset(set3))       #  чи є в set1 всі елементи які є в set3  # True  # оскільки в set1 є 1, 2, 3
 #
-# print(set1.isdisjoint(set2))        # перевіряє чи в set1 та в set2 немає спільних елементів, тоді True, якщо спільні елементи є тоді  False
+# print(set1.isdisjoint(set2))        # перевіряє чи в set1 та в set2 немає спільних елементів, тоді True, якщо
+#                                       #спільні елементи є тоді  False
 #
-# print(set1.intersection(set2))      # повертає спільні елементи, які є між двома set отримується новий set # {3, 4, 5}
-# print(set1.difference(set2))        # # повертає відмінні елементи, які є в set1 і немає в set2 отримується новий set # {1, 2, 6}
+# print(set1.intersection(set2))     # повертає спільні елементи, які є між двома set отримується новий set # {3, 4, 5}
+# print(set1.difference(set2))        # # повертає відмінні елементи, які є в set1 і немає в set2
+#                                       #отримується новий set # {1, 2, 6}
 #
 # set1.add(11)            #додасть в set1 11
 # set1.add(3)             # якби в set1 не було 3, то вона б додалась
 # print(set1)             # {1, 2, 3, 4, 5, 6, 11}
 
 
-#       # string стрінга, стрічка, пишуться в "str" або в 'str'
+#       # string стрінга, стрічка, пишуться в подвійних або одинарних лапках "str" або в 'str'
 # string1 = "text"
 # string2 = 'text'
 # string = 'name = "Vasyl"'       #   name = "Vasyl"
@@ -357,13 +365,127 @@ kkh
 
 # print(string)
 
-st = "*"*50
-
-print(st)       #   **************************************************
-
-print('hello')
-print('name')
+# st = "*"*50
+#
+# print(st)       #   **************************************************
 
 
+# name = 'Max'
+# age = 18
+# weight = 70.5
+#
+# string = 'Hello, my name is Max. I am 18 and my weight 70.5 kg' # деякі параметри винесемо в змінні
+#             # виведе Hello, my name is Max. I am 18 and my weight 70.5 kg
+# string = 'Hello, my name is '+name+ ' I am ' + str(age) + ', and my weight ' +str(weight) + 'kg'
+#                                 #int мусемо перевести в str
+#          # Hello, my name is Max I am 18, and my weight 70.5kg
+# string = 'Hello, my name is %s. I am %d and my weight %f kg' %(name, age, weight)   # %s -string, %d-digital, %-float
+#                         # Hello, my name is Max. I am 18 and my weight 70.500000 kg
+# string = 'Hello, my name is {}. I am {} and my weight {} kg'.format(name, age, weight)
+#                         # Hello, my name is Max. I am 18 and my weight 70.5 kg
+# string = 'Hello, my name is {name}. I am {age} and my weight {weight} kg'.format(name=name, age=age, weight=weight)
+#                         # Hello, my name is Max. I am 18 and my weight 70.5 kg
+# string = f'Hello, my name is {name}. I am {age} and my weight {weight} kg'
+#                         # Hello, my name is Max. I am 18 and my weight 70.5 kg
+#
+# print(string)
 
-# 1.01.04
+# print(string.index('l'))    # виведе під яким індексом буква l  резул 2
+# print(string.index('l', 3,10)) # під яким індексом буква l в діапазоні з 3 по 10 резул 3. Якщо не знайде буде помилка
+# print(string.find('l', 3,10)) # під яким індексом буква l в діапазоні з 3 по 10 резул 3. Якщо не знайде видасть
+                                # # не помилку, а -1
+# print(string.count('l'))    # виведе к-сть l в стрічці, рез 2
+
+# string = 'hello my name'
+# string = string.capitalize()    # першу букву в речені робить великою  Hello my name
+# string = string.upper()    # всі букви зробить великими  HELLO MY NAME
+# string = string.lower()    # всі букви зробить малими буквами  hello my name
+# string = string.islower()    # повертає true або false якщо вони маленькі
+# string = string.isupper()    # повертає true або false якщо вони великі
+#
+#
+# print(string)
+
+# print('hello world'.title()) # перші букви робить великими  Hello World
+# print('HEllo World'.swapcase()) # які були великими стають малими і навпаки heLLO wORLD
+# print('HEllo'.isalpha()) # перевіряє чи це букви  True
+# print('123'.isnumeric()) # перевіряє чи це цифри  True
+# print('123'.isdigit()) # перевіряє чи це цифри  True
+# print('123bgh'.isalnum()) # перевіряє чи це букви і цифри  True
+# print('hello'.endswith('lo')) # перевіряє чи закінчується на lo  True
+# print('hello'.startswith('he')) # перевіряє чи починається на he  True
+#
+# print('     ghfjf   '.strip())          # забирає всі зайві прогалини       ghfjf
+# print('aa     ghfjf   bb'.strip('ab'))          # забирає всі зайві a і b            ghfjf
+# print('aa     ghfjf   bb'.rstrip('ab'))          # забирає справа всі зайві a і b    aa     ghfjf
+# print('aa     ghfjf   bb'.lstrip('ab'))          # забирає зліва всі зайві a і b       ghfjf   bb
+# print('hello world'.split())          # заганяє стрічку в список  ['hello', 'world']
+#
+# print(list('hello world'))      # отримуємо список з букв двох слів
+
+# print('hello is hello'.partition('is'))     # partition ділить стрічку на 3 частини: до сепаратора, сепаратор
+#                                             # і після сепаратора. Рез кортеж ('hello ', 'is', ' hello')
+
+# print('hello'[::2])     # зріз з кроком 2   рез   hlo
+#
+# strs = ['hello', 'world']
+#
+# join =' '.join(strs)
+# join1 ='-'.join(strs)
+#
+#
+# print(join)         #  отримаємо hello world
+# print(join1)         #  отримаємо hello-world
+
+
+# #вбудовані ф-ції
+# print(min(8, 25, -45,  1, 3))       # видає мін значення, рез - - 45
+# print(max(8, 25, -45,  1, 3))       # видає мін значення, рез - 25
+# print(sorted([1, 3, 2, 25, -5]))     # поверне новий масив за зростанням [-5, 1, 2, 3, 25]
+# print(sorted([1, 3, 2, 25, -5], reverse=True))     # поверне новий масив зі спаданням [25, 3, 2, 1, -5]
+# print(pow(2, 3))   #  піднесення до степеня 2 в 3 степенеі, рез 8
+
+
+#  функції
+# ф-ції створюються за допомогою def називають наприк name_of_  (сней кейс)
+
+# def func(): # аргументи можна передавати або ні, тіло ф-ції пишем на 1 tab
+#     print('hello')
+#
+# func()      # hello
+#
+# def func1(a, b, c):
+#     print(a, b, c)
+#
+# func1(1, 2, 3)      #  1 2 3
+#
+# def func2(*args):       # невизначена к-сть параметрів
+#     print(args)
+#
+# func2(21, 22, 23)       # 21  22  23
+#
+#
+# def func3(d, e, *args):
+#     print(d, e, args)
+#
+# func3(-1, -2, 31, 32, 33)       # -1 -2 (31, 32, 33)
+#
+#
+# def func4(f, g, *args, **kwargs):       # kwargs -іменовані параметри - зробиться словник
+#     print(f, g)                     #  1 2
+#     print(args)                     # (41, 42, 43)
+#     print(kwargs)                   #  {'name': 'Max', 'age': 15}  словник
+#
+#
+# func4(1, 2, 41, 42, 43, name='Max', age=15)
+
+
+
+
+
+
+
+
+
+
+# 1.31.00
